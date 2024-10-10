91503fcb8e8451362f8084b8fa35e1e4e0a1364b
Ak1r4 Yuk1 <72250044+Ak1r4Yuk1@users.noreply.github.com>
2024-10-10 16:07:17 +0200

    Update split.cmd

--- split.cmd
+++ split.cmd
@@ -8,5 +8,4 @@ set /p filename=<data.filename
 ffmpeg -i "!filename!.mp4" -c copy -map 0 -segment_time 2700 -f segment -reset_timestamps 1 "!filename! PART%%03d.mp4"
 
 :Comando per eliminare i files non necessari
-del /f /q "data.filename"
 rmdir /s /q Logs
